3363372c66fd1b506c519e93a4b8db111fa077e3
ElephantOH <74961300+ElephantOH@users.noreply.github.com>
2026-06-26 22:10:17 +0800

    Update fast_run.py

--- fast_run.py
+++ fast_run.py
@@ -2,15 +2,12 @@ from src.tools.close_domain_dqa.mab_retrieval_tools import MABRetrieverTools
 
 if __name__ == "__main__":
     retriever = MABRetrieverTools(
-        retriever_type="colpali/colpali-1.3",
-        vlm_type="qwen/qwen25vl-7b",
+        retriever="colpali/colpali-1.3",
+        vlm="qwen/qwen25vl-7b",
         top_k=10,
     )
-    while True:
-        print("\n" + "-"*50)
-        pdf_path = "examples/example.pdf"
-        question = "what is total current assets in FY2023 for Bestbuy? Answer in million."
-        try:
-            pages, scores = retriever.retrieve(pdf_path, question)
-        except Exception as e:
-            print(f"Error: {e}")
+    
+    pdf_path = "examples/example.pdf"
+    question = "what is total current assets in FY2023 for Bestbuy? Answer in million."
+    
+    pages, scores = retriever.retrieve(pdf_path, question)
